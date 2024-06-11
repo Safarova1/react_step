366437ecf7b7eda096d6e8c6227a563aee2f2d1f
HP <safarova_s@itstep.academy>
2024-06-11 20:53:00 +0400

    first project

---
+++ lesson2/src/App.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+// import Header from "./components/Header";
+// import Product from "./components/Product";
+import Booklist from "./components/Booklist";
+
+// PRIMER 1
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <Header isAuto={false} title="zaqolovka" />
+//       <h1>Main title</h1>
+//       {/* <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est et,
+//         perspiciatis cum ex dolorum aut dolores nemo odio, minima laboriosam
+//         dolor suscipit amet dicta vitae eaque deleniti iure, quisquam
+//         blanditiis!
+//       </p> */}
+//       <Product
+//         name="T-shirt"
+//         description="Lorem ipsum dolor sit"
+//         imgsrc="https://ic.pics.livejournal.com/miss_hohotyn007/73968878/4561532/4561532_original.jpg"
+//       />
+
+//       <Product
+//         name="T-shirt"
+//         description="Lorem ipsum dolor sit"
+//         imgsrc="https://ic.pics.livejournal.com/miss_hohotyn007/73968878/4561532/4561532_original.jpg"
+//       />
+//     </React.Fragment>
+//   );
+// };
+
+// export default App;
+
+// -----------------------------------------------------------------------------------------------
+// PRIMER 2 3
+// const products = [
+//   {
+//     title: "Wireless Earbuds",
+//     description: "High-quality wireless earbuds with noise-canceling feature.",
+//     price: 49.99,
+//   },
+//   {
+//     title: "Smart Watch",
+//     description:
+//       "Multifunctional smart watch with fitness tracking and notifications.",
+//     price: 199.99,
+//   },
+//   {
+//     title: "Bluetooth Speaker",
+//     description:
+//       "Portable Bluetooth speaker with powerful sound and long battery life.",
+//     price: 89.99,
+//   },
+//   {
+//     title: "Gaming Laptop",
+//     description:
+//       "High-performance gaming laptop with advanced graphics and fast processor.",
+//     price: 1299.99,
+//   },
+//   {
+//     title: "4K Ultra HD TV",
+//     description:
+//       "55-inch 4K Ultra HD TV with smart features and vibrant display.",
+//     price: 499.99,
+//   },
+//   {
+//     title: "Fitness Tracker",
+//     description:
+//       "Wearable fitness tracker with heart rate monitor and activity tracking.",
+//     price: 59.99,
+//   },
+//   {
+//     title: "Instant Pot",
+//     description:
+//       "7-in-1 programmable pressure cooker, slow cooker, rice cooker, and more.",
+//     price: 79.99,
+//   },
+//   {
+//     title: "Electric Toothbrush",
+//     description:
+//       "Rechargeable electric toothbrush with multiple brushing modes.",
+//     price: 39.99,
+//   },
+//   {
+//     title: "Noise Cancelling Headphones",
+//     description:
+//       "Over-ear noise-canceling headphones with high-fidelity sound.",
+//     price: 149.99,
+//   },
+//   {
+//     title: "Smart Home Hub",
+//     description:
+//       "Smart home hub to control your smart devices with voice commands.",
+//     price: 129.99,
+//   },
+// ];
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header isAuto={false} title="zaqolovka" />
+//       <h1>Main title</h1>
+
+//       {products.map((product) => (
+//         <Product
+//           key={product.title}
+//           title={product.title}
+//           description={product.description}
+//           price={product.price}
+//         />
+//       ))}
+//     </>
+//   );
+// };
+
+// export default App;
+
+// -----------------------------------------------------------------------------------------------
+// zadanie 1
+
+// Создать компонент BookList
+// Будет рендерить список книг
+// Создать компонент для разметки книги
+// если массив пустой - нету книг
+
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+];
+
+const App = () => {
+  return (
+    <>
+      <h1>Booklist</h1>
+
+      <Booklist book={favouriteBooks} />
+    </>
+  );
+};
+
+export default App;
---
+++ lesson2/src/components/Product.jsx
@@ -0,0 +1,55 @@
+// PRIMER 1
+// import React from "react";
+
+// const Product = (props) => {
+//   console.log("props", props);
+//   return (
+//     <div>
+//       <h2>Product</h2>
+//       <h4>{props.name}</h4>
+//       <h4>{props.description}</h4>
+//       <img src={props.imgsrc} alt="" />
+//     </div>
+//   );
+// };
+
+// export default Product;
+
+// -----------------------------------------------------------------------------------------------
+
+// PRIMER 2
+// import React from "react";
+
+// const Product = (props) => {
+//   return (
+//     <div>
+//       <h2>Product</h2>
+//       <h4>{props.title}</h4>
+//       <p>{props.description}</p>
+//       <button>{props.price}</button>
+//     </div>
+//   );
+// };
+
+// export default Product;
+
+
+
+
+
+
+// PRIMER 3
+// import React from "react";
+
+// const Product = ({title,description,price}) => {
+//   return (
+//     <div>
+//       <h2>Product</h2>
+//       <h4>{title}</h4>
+//       <p>{description}</p>
+//       <button>{price}</button>
+//     </div>
+//   );
+// };
+
+// export default Product;
