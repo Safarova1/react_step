03a219d20adc2284f5e01edf6cf9be159ca1ae04
HP <safarova_s@itstep.academy>
2024-06-14 00:52:07 +0400

    lesson2

---
+++ lesson2/src/App.jsx
@@ -0,0 +1,43 @@
+import Authentication from "./components/Authentication";
+import Card from "./components/Card";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import LoginForm from "./components/LoginForm";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      {/* <Header title="hello" /> */}
+      <Authentication />
+      {/* <Counter /> */}
+      <Card>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+        adipisci. Qui atque modi accusantium animi voluptates ullam quis
+        explicabo dolorum.
+      </Card>
+      {/* <Card>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+        adipisci. Qui atque modi accusantium animi voluptates ullam quis
+        explicabo dolorum.
+      </Card>
+      <Header />
+      <Card>
+        <h2>Card Title</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </Card> */}
+      {/* <div
+        onScroll={(e) => {
+          console.log("e :>> ", e);
+        }}
+        onClick={(event) => {
+          console.log("clicked on div", event);
+        }}
+      >
+        hello
+      </div> */}
+    </>
+  );
+}
+
+export default App;
---
+++ lesson2/src/components/SignupForm.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+class SignupForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <label htmlFor="username">Enter your username</label>
+        <input
+          type="text"
+          name="username"
+          onChange={this.props.handleUsernameChange}
+          value={this.props.username}
+        />
+        <label>Enter your password</label>
+        <input
+          type="text"
+          name="password"
+          onChange={this.props.handlePasswordChange}
+          value={this.props.password}
+        />
+        <label>Repeat your password</label>
+        <input
+          type="text"
+          name="password"
+          onChange={this.props.handlePasswordChange}
+          value={this.props.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
