092b2272e41c77ddc9135f8c95bd67a475b74790
HP <safarova_s@itstep.academy>
2024-06-22 03:20:34 +0400

    zadacu ewe raz rewila sama

--- lesson2/src/components/Counter.jsx
+++ lesson2/src/components/Counter.jsx
@@ -44,3 +44,43 @@ export default Counter;
 // который будет иметь булевое состояние
 // Добавить кнопку для переклюения состояния
 // показывать компонент с текстом 'Modal Open' в зависимости от состояния
+
+// sama rewila 2oy sposob
+// import React, { Component } from "react";
+
+// export default class Product extends Component {
+//   state = {
+//     value: 0,
+//     active: true,
+//   };
+
+//   minus = () => {
+//     this.setState({
+//       value: this.state.value - 1,
+//       active: (this.state.active = false),
+//     });
+//   };
+
+//   plus = () => {
+//     this.setState({
+//       value: this.state.value + 1,
+//       active: (this.state.active = true),
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>{this.state.value}</h2>
+//         {this.state.active == true ? (
+//           <p> uvelicivayetsya</p>
+//         ) : (
+//           <p>umenshayetysa </p>
+//         )}
+
+//         <button onClick={this.plus}>+</button>
+//         <button onClick={this.minus}>-</button>
+//       </div>
+//     );
+//   }
+// }
